e0fa4e4278a8bb9e94fc9631f1637b4cc54248e4
Mina Botros <minabotros46@gmail.com>
2020-04-02 19:24:42 +0200

    Add files via upload

---
+++ Day2.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Testing
+		System.out.println(isHappy(19));
+	}
+	
+	public static boolean isHappy(int num) {
+		
+		int sum;
+		for (int i = 0 ; i < 10 ; i++) {
+			sum = 0;
+			while (num != 0) {
+				int tmp = num % 10;
+				num = num / 10;
+				sum += Math.pow(tmp, 2);
+			}
+			if (sum == 1)
+				return true;
+			num = sum;
+		}
+        return false;
+    }
+
+}
